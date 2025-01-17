ee84088e2721292f7daa3c238f93bb9e5e5cba7e
LadAkanksha <akankshalad2003@gmail.com>
2025-01-17 23:10:21 +0530

    Apna College

---
+++ JavaScript/Practice Program/Practice Set/Exercise 1/script.js
@@ -0,0 +1,22 @@
+
+// Q.1)
+
+const pen = {
+     productName : "Parker Jotter Standard CT Ball Pen",
+    ink_color:"black",
+    rating: 4,
+    price:270,
+    discount:5
+};
+
+// Q.2)
+
+const profile = {
+    userName : "shradhakhapra",
+    isFollow : true,
+    isMessage : false,
+    posts : 195,
+    follwers : "569K",
+    followinf : 4,
+    details : "Apna College | Ex-Microsoft,DRDO To educate someone is the highest privilege"
+};
