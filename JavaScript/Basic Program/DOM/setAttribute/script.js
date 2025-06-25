03babf2d9e3dbd78f8b525bad753af2e36d1df63
LadAkanksha <akankshalad2003@gmail.com>
2025-06-25 23:41:57 +0530

    DOM

---
+++ JavaScript/Basic Program/DOM/setAttribute/script.js
@@ -0,0 +1,9 @@
+
+//Access the element by querySelector()
+let div = document.querySelector("div");
+console.log(div);
+
+// Change/Set the attribute value by setAttribute()
+div.setAttribute("id","newId");
+div.setAttribute("class","newClass");
+console.log(div);
